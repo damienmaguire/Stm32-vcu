96b886cfd0e44cc2036575fd0ed813eb4ba8497b
Damien Maguire <listerlt1@gmail.com>
2022-11-14 13:05:52 +0000

    Update Ver and 450h
    -Update the reported version to 1.01.
    -Add use of PWMs as digital op/p
    -Verify GS450h driving in vehicle

--- include/param_prj.h
+++ include/param_prj.h
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define VER 1.00.A
+#define VER 1.01.A
 
 
 /* Entries must be ordered as follows:
