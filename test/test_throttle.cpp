d986120e26eaadc3035256cc41653b3a317e8624
Jamie Jones <jamie@mudslidedesign.co.uk>
2022-11-18 21:20:07 +0000

    Adding first unit tests, testing the Throttle TemperatureDerate function

---
+++ test/test_throttle.cpp
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the tumanako_vc project.
+ *
+ * Copyright (C) 2010 Johannes Huebner <contact@johanneshuebner.com>
+ * Copyright (C) 2010 Edward Cheeseman <cheesemanedward@gmail.com>
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "my_fp.h"
+#include "my_math.h"
+#include "test_list.h"
+#include "throttle.h"
+
+using namespace std;
+
+static void TestSetup()
+{
+
+}
+
+static void TestThrottleTemperateOverMaxThrottleTo0() {
+   float temp = 60;
+   float tempMax = 50;
+   float finalSpnt = 100;
+   ASSERT(Throttle::TemperatureDerate(temp, tempMax, finalSpnt) && finalSpnt == 0);
+}
+
+static void TestThrottleTemperateInDerateZoneThrottleTo50Percent() {
+   float temp = 61;
+   float tempMax = 60;
+   float finalSpnt = 100;
+   ASSERT(Throttle::TemperatureDerate(temp, tempMax, finalSpnt) && finalSpnt == 50);
+}
+
+static void TestThrottleUnderTemperateNoDeRate() {
+   float temp = 30;
+   float tempMax = 60;
+   float finalSpnt = 100;
+   ASSERT(Throttle::TemperatureDerate(temp, tempMax, finalSpnt) == false && finalSpnt == 100);
+}
+
+void ThrottleTest::RunTest()
+{
+   TestSetup();
+   TestThrottleTemperateOverMaxThrottleTo0();
+   TestThrottleTemperateInDerateZoneThrottleTo50Percent();
+   TestThrottleUnderTemperateNoDeRate();
+}
