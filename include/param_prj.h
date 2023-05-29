7029b3ccb35380c622e83f0f330bab883a0289ff
Damien Maguire <listerlt1@gmail.com>
2023-05-29 12:26:17 +0100

    Bench test bms
    -Bench test current bms does not affect normal op.
    
    -uprev to 2.01
    
    -add codeblocks mods

--- include/param_prj.h
+++ include/param_prj.h
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#define VER 2.00.A
+#define VER 2.01.A
 
 
 /* Entries must be ordered as follows:
