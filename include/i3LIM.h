ed70478e92a4167259f729fd1783420255f5b608
David J. Fiddes <D.J@fiddes.net>
2024-09-27 14:28:19 +0100

    Simplify i3LIMClass interface
    Remove the static designator from private methods in
    i3LIMClass. This gives the optimizer more
    opportunities to inline methods. The data should
    probably be moved to be class members but that is
    too intrusive at this time.
    
    Mark overridden virtual methods with "override" to
    reduce likelihood of future breakage should the
    parent method signature change.
    
    Tidy the includes.

--- include/i3LIM.h
+++ include/i3LIM.h
@@ -5,33 +5,29 @@
 
 */
 
-#include <stdint.h>
-#include "my_fp.h"
-#include "params.h"
-#include "stm32_can.h"
 #include "chargerint.h"
-#include "my_math.h"
+#include "canhardware.h"
+#include <stdint.h>
 
 class i3LIMClass: public Chargerint
 {
-
 public:
-      void SetCanInterface(CanHardware* c);
-      void DecodeCAN(int id, uint32_t data[2]);
-      void Task10Ms();
-      void Task100Ms();
-      void Task200Ms();
-      bool DCFCRequest(bool RunCh);
-      bool ACRequest(bool RunCh);
+      void SetCanInterface(CanHardware* c) override;
+      void DecodeCAN(int id, uint32_t* data) override;
+      void Task10Ms() override;
+      void Task100Ms() override;
+      void Task200Ms() override;
+      bool DCFCRequest(bool RunCh) override;
+      bool ACRequest(bool RunCh) override;
 
 private:
-static void handle3B4(uint32_t data[2]);
-static void handle29E(uint32_t data[2]);
-static void handle2B2(uint32_t data[2]);
-static void handle2EF(uint32_t data[2]);
-static void handle272(uint32_t data[2]);
-static void CCS_Pwr_Con();
-static void Chg_Timers();
+      void handle3B4(uint32_t data[2]);
+      void handle29E(uint32_t data[2]);
+      void handle2B2(uint32_t data[2]);
+      void handle2EF(uint32_t data[2]);
+      void handle272(uint32_t data[2]);
+      void CCS_Pwr_Con();
+      void Chg_Timers();
 };
 
 #endif /* i3LIM_h */
