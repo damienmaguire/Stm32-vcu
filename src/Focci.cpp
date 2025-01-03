a4135cbdca0b7068570dd916f1f9a3782c9162e9
tomdebree <tomdebree@hotmail.com>
2025-01-03 18:56:04 +0100

    Add DCFC CCS support
    Add the logic to regonise Foccci wants to attempt DC fast charging.
    
    Puts Zombie in Charge mode and then runs current calculations

--- src/Focci.cpp
+++ src/Focci.cpp
@@ -6,13 +6,12 @@ static uint8_t ChargePort_Plug = 0;
 static uint8_t ChargePort_Lock = 0;
 
 static bool ChargePort_ReadyCharge = false;
+static bool ChargePort_ReadyDCFC = false;
 static bool PlugPres = false;
 static bool RX_357Pres = false;
 static bool ChargeAllow = false;
 
 static uint8_t AcOBCReq = 0;
-
-static bool DcFastCharging = false;
 
 static uint8_t CP_Mode=0;
 static uint8_t Timer_1Sec=0;
@@ -150,6 +149,15 @@ void FocciClass::handle357(uint32_t data[2])  //FOCCI Charge Port Info
         ChargePort_ReadyCharge = false;
     }
 
+    if(ChargePort_Status == 0x04)//check DC connected and ready to attempt charge
+    {
+        ChargePort_ReadyDCFC = true;
+    }
+    else
+    {
+        ChargePort_ReadyDCFC = false;
+    }
+
 
     //0=Absent, 1=ACStd, 2=ACchg, 3=Error
     CP_Mode = ChargePortStatus;
@@ -221,7 +229,7 @@ void FocciClass::Task200Ms()
 
 void FocciClass::Task100Ms()
 {
-    if(DcFastCharging)
+    if(ChargePort_ReadyDCFC)
     {
         CCS_Pwr_Con(); //Calc DC current req
     }
@@ -278,8 +286,15 @@ void FocciClass::Chg_Timers()
 
 bool FocciClass::DCFCRequest(bool RunCh)
 {
+    if(ChargePort_ReadyDCFC == true)
+    {
+        return true; //Attempt DC Charging session
+    }
+    else
+    {
+        return false; //No DC Charging session
+    }
     RunCh = RunCh;
-    return false; //No DC Charging support
 }
 
 bool FocciClass::ACRequest(bool RunCh)
