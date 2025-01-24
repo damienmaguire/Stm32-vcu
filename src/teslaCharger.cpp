91765673808c23f929a0b97d7384d04eeb5937e6
Christian Kelly <chrskly@chrskly.com>
2025-01-24 22:23:18 +0000

    More whitespace

--- src/teslaCharger.cpp
+++ src/teslaCharger.cpp
@@ -31,34 +31,33 @@ void teslaCharger::DecodeCAN(int id, uint32_t data[2])
 
 void teslaCharger::Task100Ms()
 {
-   uint8_t bytes[8];
-   HVvolts = Param::GetInt(Param::udc);
-   HVspnt = Param::GetInt(Param::Voltspnt);
-   HVpwr = Param::GetInt(Param::Pwrspnt);
-   calcBMSpwr = (HVvolts * Param::GetInt(Param::BMS_ChargeLim));  // BMS charge current limit but needs to be power for most AC charger types.
-   HVpwr = MIN(HVpwr, calcBMSpwr);
-   bytes[0] = Param::GetInt(Param::opmode);  // operation mode
-   bytes[1] = (HVvolts & 0xFF);  // HV voltage lowbyte
-   bytes[2] = ((HVvolts & 0xFF00) >> 8);  // HV voltage highbyte
-   bytes[3] = (HVspnt & 0xFF);  // HV voltage setpoint lowbyte
-   bytes[4] = ((HVspnt & 0xFF00) >> 8);  // HV voltage setpoint highbyte
-   bytes[5] = (HVpwr & 0xFF);  // HV voltage power setpoint lowbyte
-   bytes[6] = ((HVpwr & 0xFF00) >> 8);  // HV voltage power setpoint highbyte
+  uint8_t bytes[8];
+  HVvolts = Param::GetInt(Param::udc);
+  HVspnt = Param::GetInt(Param::Voltspnt);
+  HVpwr = Param::GetInt(Param::Pwrspnt);
+  calcBMSpwr = (HVvolts * Param::GetInt(Param::BMS_ChargeLim));  // BMS charge current limit but needs to be power for most AC charger types.
+  HVpwr = MIN(HVpwr, calcBMSpwr);
+  bytes[0] = Param::GetInt(Param::opmode);  // operation mode
+  bytes[1] = (HVvolts & 0xFF);  // HV voltage lowbyte
+  bytes[2] = ((HVvolts & 0xFF00) >> 8);  // HV voltage highbyte
+  bytes[3] = (HVspnt & 0xFF);  // HV voltage setpoint lowbyte
+  bytes[4] = ((HVspnt & 0xFF00) >> 8);  // HV voltage setpoint highbyte
+  bytes[5] = (HVpwr & 0xFF);  // HV voltage power setpoint lowbyte
+  bytes[6] = ((HVpwr & 0xFF00) >> 8);  // HV voltage power setpoint highbyte
   if (ChRun)bytes[7] = ((0xA << 4 ) | counter_109);  // send vcu enable
   if (!ChRun)bytes[7] = ((0xC << 4 ) | counter_109);  // send vcu disable
-   counter_109++;
-   if(counter_109 >= 0xF) counter_109 = 0;
-   can->Send(0x109, (uint32_t*)bytes, 8);
+  counter_109++;
+  if(counter_109 >= 0xF) counter_109 = 0;
+  can->Send(0x109, (uint32_t*)bytes, 8);
 }
 
 bool teslaCharger::ControlCharge(bool RunCh, bool ACReq)
 {
-   bool dummy = RunCh;
-   dummy = dummy;
-   ChRun = ACReq;
-   if (HVreq) return true;
-   if (!HVreq) return false;
-   return false;
-
+  bool dummy = RunCh;
+  dummy = dummy;
+  ChRun = ACReq;
+  if (HVreq) return true;
+  if (!HVreq) return false;
+  return false;
 }
 
