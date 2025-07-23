ec3e57bd97777c58973c29394fa5c47c4f60b88c
tomdebree <tomdebree@hotmail.com>
2025-07-23 15:05:16 +0100

    Remove throtramprpm

--- src/utils.cpp
+++ src/utils.cpp
@@ -471,15 +471,7 @@ float ProcessThrottle(int speed)
 {
     float finalSpnt;
 
-    if (speed < Param::GetInt(Param::throtramprpm))
-    {
-        Throttle::throttleRamp = Param::GetFloat(Param::throtramp);
-    }
-
-    else
-    {
-        Throttle::throttleRamp = Param::GetAttrib(Param::throtramp)->max;
-    }
+    Throttle::throttleRamp = Param::GetFloat(Param::throtramp);
 
     finalSpnt = utils::GetUserThrottleCommand();
 
