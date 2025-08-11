1c5f079b7e5f31d434af63c25ba6057a473a2343
Ben Bament <bbament@gmail.com>
2025-08-11 16:09:08 +0100

    Update simpbms.h

--- include/simpbms.h
+++ include/simpbms.h
@@ -43,7 +43,7 @@ private:
     float stateOfCharge = 0;
     float current = 0;
     float remainingKHW = 0;
-    float batteryVoltage = 500; //higher than possible so cannot complete precharge until BMS reports battery voltage#
-    float rawCurrent = 0;
+    float batteryVoltage = 500; //higher than possible so cannot complete precharge until BMS reports battery voltage
+
 };
 #endif // SIMPBMS_H
