34e1d9311b98f9ae25c1e295ccd8edd563708582
Damien Maguire <listerlt1@gmail.com>
2025-11-08 15:14:50 +0000

    Fix ipace shifter bug

--- src/JLR_iP.cpp
+++ src/JLR_iP.cpp
@@ -25,7 +25,7 @@ Standard CAN at 500kbps.
 The GSM is connected to the PMZ (Power Mode Zero) can bus on the ipace which uses a convoluted CRC8 with salt bytes.
 As the shifter only has 4 states (PRND) and the changes between messages are small I elected to just use a lookup table to replay the required CRC byte.
 
-Optional to send CAN id 0x178 at 100ms to set backlight on/off and backlight intensity. Implemented in this sketch.
+Optional to send CAN id 0x178 at 100ms to set backlight on/off and backlight intensity.
 
 Part numbers for the shifter : J9D3-14C559-DA , J9D3-14C561-BB , J9D3-7E453-DC.
 
@@ -41,17 +41,11 @@ Now you can say : I drive a Jaaaaag!!!
 
 */
 
-#define iPark 0;
-#define iReverse 1;
-#define iNeutral 2;
-#define iDrive 3;
-
 
 uint8_t Count48=0;//0=16 counter for message 0x048 in byte 4
-int8_t selectedGear = 0;
 uint8_t receivedGear = 2;
 uint8_t timer20 = 0;
-uint8_t BackLightMapped;
+uint8_t BackLightMapped = 0xFD;//Fixed backlight value for now
 //////////////CRC lookups for the 4 gear states//////////////////////////////////////////////////
 const uint8_t CycleP[16]={0x21,0xbc,0x06,0x9b,0x6f,0xf2,0x48,0xd5,0xbd,0x20,0x9a,0x07,0xf3,0x6e,0xd4,0x49};
 const uint8_t CycleR[16]={0x67,0xfa,0x40,0xdd,0x29,0xb4,0x0e,0x93,0xfb,0x66,0xdc,0x41,0xb5,0x28,0x92,0x0f};
@@ -76,9 +70,29 @@ void JLR_iP::DecodeCAN(int id, uint32_t* data)
     uint8_t* bytes = (uint8_t*)data;
     if (id == 0x115)
     {
-      receivedGear = (bytes[4] >>4);
-    }
+      receivedGear = (bytes[4] >>4);//Park =0 , Reverse = 1, Neutral =2, Drive=3
 
+    switch (receivedGear) {
+      case 0://Park
+      this->gear = PARK;
+      break;
+
+      case 1://Reverse
+      this->gear = REVERSE;
+      break;
+
+      case 2://Neutral
+      this->gear = NEUTRAL;
+      break;
+
+      case 3://Drive
+      this->gear = DRIVE;
+      break;
+
+      default:
+      break;
+    }
+    }
 }
 
 
@@ -96,34 +110,30 @@ uint8_t bytes[8];
 
    if(opmode==MOD_RUN)
    {
-        switch (selectedGear) {
+        switch (selectedDir) {
       case 2://Park
         bytes[0]=0x00;// (0x04 , 0x00 Drive) , (0x02 , 0x00 Neutral) , (0x01 , 0x00 Reverse) , (0x00, 0x80 Park)
         bytes[1]=0x80;//second byte of display
         bytes[3]=0x58;//0x40 for drive, 0x58 for park, 0x44 for neutral
         bytes[2]=CycleP[Count48];//CRC via lookup tables per mode
-        this->gear = PARK;
         break;
       case -1://Reverse
         bytes[0]=0x01;// (0x04 , 0x00 Drive) , (0x02 , 0x00 Neutral) , (0x01 , 0x00 Reverse) , (0x00, 0x80 Park)
         bytes[1]=0x00;//second byte of display
         bytes[3]=0x40;//0x40 for drive, 0x58 for park, 0x44 for neutral
         bytes[2]=CycleR[Count48];//CRC via lookup tables per mode
-        this->gear = REVERSE;
         break;
       case 0://Neutral
         bytes[0]=0x02;// (0x04 , 0x00 Drive) , (0x02 , 0x00 Neutral) , (0x01 , 0x00 Reverse) , (0x00, 0x80 Park)
         bytes[1]=0x00;//second byte of display
         bytes[3]=0x44;//0x40 for drive, 0x58 for park, 0x44 for neutral
         bytes[2]=CycleN[Count48];//CRC via lookup tables per mode
-        this->gear = NEUTRAL;
         break;
       case 1://Drive
         bytes[0]=0x04;// (0x04 , 0x00 Drive) , (0x02 , 0x00 Neutral) , (0x01 , 0x00 Reverse) , (0x00, 0x80 Park)
         bytes[1]=0x00;//second byte of display
         bytes[3]=0x40;//0x40 for drive, 0x58 for park, 0x44 for neutral
         bytes[2]=CycleD[Count48];//CRC via lookup tables per mode
-        this->gear = DRIVE;
         break;
       default:
         // statements
