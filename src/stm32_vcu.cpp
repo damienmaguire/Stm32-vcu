b5be87db5c66e2099649ec2755263b92df4e5a37
bratindustries <angusjohnson@protonmil.com>
2026-08-20 21:47:31 -0600

    water pump precharge hang fix/ delay

--- src/stm32_vcu.cpp
+++ src/stm32_vcu.cpp
@@ -6,6 +6,7 @@
  * Copyright (C) 2010 Edward Cheeseman <cheesemanedward@gmail.com>
  * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
  * Copyright (C) 2019-2022 Damien Maguire <info@evbmw.com>
+ * Changes by Angus Johnson 2026 <info@bratindustries.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -148,6 +149,8 @@ static volatile unsigned days = 0, hours = 0, minutes = 0, seconds = 0,
 
 static uint16_t rlyDly = 25;
 static uint16_t prechargeMinTime = 100;
+static const uint16_t COOLANT_PUMP_DELAY = 25; // 250 ms in the 10 ms task
+static uint16_t coolantPumpDelay = COOLANT_PUMP_DELAY;
 
 // Instantiate Classes
 static BMW_E31 e31Vehicle;
@@ -755,7 +758,8 @@ static void Ms10Task(void) {
       DigIo::inv_out.Set(); // inverter power on
     }
     IOMatrix::GetPinOut(IOMatrix::NEGCONTACTOR)->Set();
-    IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Set();
+    IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Clear();
+    coolantPumpDelay = COOLANT_PUMP_DELAY;
     if (rlyDly != 0)
       rlyDly--; // here we are going to pause before energising precharge to
                 // prevent too many contactors pulling amps at the same time
@@ -802,6 +806,8 @@ static void Ms10Task(void) {
 
   case MOD_PCHFAIL:
     StartSig = false;
+    IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Clear();
+    coolantPumpDelay = COOLANT_PUMP_DELAY;
     DigIo::prec_out
         .Clear(); // explicitly turn off precharge relay in a fail condition
     if (initbyCharge && !chargeMode)
@@ -819,6 +825,12 @@ static void Ms10Task(void) {
                 // prevent too many contactors pulling amps at the same time
     if (rlyDly == 0) {
       DigIo::dcsw_out.Set();
+      if (coolantPumpDelay != 0) {
+        coolantPumpDelay--;
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Clear();
+      } else {
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Set();
+      }
     }
     ErrorMessage::UnpostAll();
     if (!chargeMode) {
@@ -835,6 +847,12 @@ static void Ms10Task(void) {
     if (rlyDly == 0) {
       DigIo::dcsw_out.Set();
       DigIo::inv_out.Set(); // inverter power on
+      if (coolantPumpDelay != 0) {
+        coolantPumpDelay--;
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Clear();
+      } else {
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Set();
+      }
     }
     Param::SetInt(Param::opmode, MOD_RUN);
     ErrorMessage::UnpostAll();
@@ -851,6 +869,12 @@ static void Ms10Task(void) {
                 // prevent too many contactors pulling amps at the same time
     if (rlyDly == 0) {
       DigIo::dcsw_out.Set();
+      if (coolantPumpDelay != 0) {
+        coolantPumpDelay--;
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Clear();
+      } else {
+        IOMatrix::GetPinOut(IOMatrix::COOLANTPUMP)->Set();
+      }
     }
 
     preheater.Ms10Task();
